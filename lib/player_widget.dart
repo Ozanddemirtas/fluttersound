ad8edf2b2b7ae5c52fcd17a02b699c13417d774b
Ozan Deniz Demirtas <ozanddemirtas@gmail.com>
2022-11-20 23:50:42 +0300

    player changed

---
+++ lib/player_widget.dart
@@ -0,0 +1,105 @@
+import 'package:audioplayers/audioplayers.dart';
+import 'package:flutter/material.dart';
+
+class PlayerWidget extends StatefulWidget {
+  final String url;
+  const PlayerWidget({
+    Key? key,
+    required this.url,
+  }) : super(key: key);
+
+  @override
+  State<PlayerWidget> createState() => _PlayerWidgetState();
+}
+
+class _PlayerWidgetState extends State<PlayerWidget>
+    with TickerProviderStateMixin {
+  late AudioPlayer player;
+  late bool initialized;
+  late AnimationController _animationController;
+  Duration? duration;
+  Duration position = Duration.zero;
+  @override
+  void initState() {
+    super.initState();
+    initialized = false;
+    _animationController =
+        AnimationController(vsync: this, duration: Duration(milliseconds: 450));
+    init();
+  }
+
+  Future setDuration() async {
+    duration = await player.getDuration();
+  }
+
+  //as
+  Future<void> init() async {
+    player = AudioPlayer()..setReleaseMode(ReleaseMode.stop);
+    await player.setSourceDeviceFile(widget.url);
+    await setDuration();
+    player.onPositionChanged.listen((event) {
+      position = event;
+      print(position);
+      setState(() {});
+    });
+    Future.microtask(() {
+      initialized = true;
+      setState(() {});
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return initialized
+        ? Column(
+            children: [
+              Row(
+                children: [
+                  IconButton(
+                      onPressed: () {
+                        setState(() async {
+                          if (player.state == PlayerState.playing) {
+                            await player.pause();
+                            _animationController.reverse();
+                          } else if (player.state == PlayerState.completed ||
+                              player.state == PlayerState.stopped) {
+                            await player.play(DeviceFileSource(widget.url));
+                            // await player.play(BytesSource(File(widget.url).readAsBytesSync()));
+                            // await player.seek(Duration(seconds: 1));
+                            _animationController.forward();
+                          } else {
+                            await player.resume();
+                            _animationController.forward();
+                          }
+                        });
+                      },
+                      icon: AnimatedIcon(
+                        icon: AnimatedIcons.play_pause,
+                        progress: _animationController,
+                      )),
+                  if (duration != null)
+                    Expanded(
+                      child: Slider(
+                        onChanged: (value) async {
+                          final _position = Duration(seconds: value.toInt());
+                          await player.seek(_position);
+                        },
+                        value: position.inSeconds.toDouble(),
+                        max: duration!.inSeconds.toDouble(),
+                        min: 0,
+                      ),
+                    ),
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Text(position.toString()),
+                  Text((duration! - position).toString())
+                ],
+              )
+            ],
+          )
+        : Container();
+  }
+}
